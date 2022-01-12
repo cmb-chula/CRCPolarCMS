bea4143dbe3d630555ac08a3b0c9c7524a22384f
Dhup Bhukdee <64527768+Delta22627@users.noreply.github.com>
2022-01-12 15:54:08 +0700

    Update to support file location

--- directional_gsea.py
+++ directional_gsea.py
@@ -7,14 +7,14 @@ import gseapy as gp
 
 entrez_to_symbol = {}
 symbol_to_entrez = {}
-df = pd.read_csv('./support_file/df.csv', header = 0, index_col = 0)
+df = pd.read_csv('df, header = 0, index_col = 0, compression='gzip')
 cms_cmap = {'CMS1':'tab:green', 'CMS2_1':'tab:red', 'CMS2_2':'tab:purple', 'CMS3':'tab:brown', \
             'CMS4_1':'tab:pink', 'CMS4_2':'tab:olive', 'CMS4_3':'tab:cyan'}
-directional_pearson_correlation = pd.read_csv('./support_file/directional_pearson_correlation.csv', header = 0, index_col = 0)
+directional_pearson_correlation = pd.read_csv('directional_pearson_correlation.csv', header = 0, index_col = 0)
 directional_pearson_correlation.columns = [float(x) for x in directional_pearson_correlation.columns]
 directional_pearson_correlation.index = [str(x) for x in directional_pearson_correlation.index]
 
-with gzip.open('./support_file/biomart.txt.gz', 'rt') as fin:
+with gzip.open('biomart.txt.gz', 'rt') as fin:
     fin.readline()
  
     for line in fin.readlines():
@@ -78,7 +78,7 @@ def plot_directional_gsea(gsea, term, fdr = 0.05, linewidth = 4, display = False
     plt.show()
     
 def directional_gsea(term , target_genes, fdr = 0.05, linewidth = 4, display = True):
-    gsea_results = pickle.load(open('./support_file/gsea_custom_genes_103121.pkl', 'rb'))
+    gsea_results = pickle.load(open('gsea_custom_genes_103121.pkl', 'rb'))
     if term in gsea_results[0.06283185307179529].res2d.index:
         print(term)
         print(f"Given term ({term}) is in precompute geneset. Using precompute data.")
@@ -91,8 +91,7 @@ def directional_gsea(term , target_genes, fdr = 0.05, linewidth = 4, display = T
         for s in gsea_genesets:
             gsea_genesets_symbols[s] = [entrez_to_symbol[g] for g in gsea_genesets[s]]
         for angle in directional_pearson_correlation.columns.values:
-            temp = pd.concat([pd.DataFrame(directional_pearson_correlation.index.values, index = directional_pearson_correlation.index), \
-                              directional_pearson_correlation[angle]], axis = 1)
+            temp = pd.concat([pd.DataFrame(directional_pearson_correlation.index.values, index = directional_pearson_correlation.index), directional_pearson_correlation[angle]], axis = 1)
             gsea_results[angle] = gp.prerank(rnk = temp, gene_sets = gsea_genesets,
                                              processes = 4,
                                              permutation_num = 1000,
